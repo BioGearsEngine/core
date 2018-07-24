baee7f3747efd8d04bec17df478b00e4bd4a36c6
Nathan Tatum <ntatum@ara.com>
2018-07-24 11:55:22 -0400

    Update to give warning when exceeding maximum work rate

--- projects/biogears/src/engine/Systems/Energy.cpp
+++ projects/biogears/src/engine/Systems/Energy.cpp
@@ -225,6 +225,7 @@ void Energy::Exercise()
       exerciseIntensity = DesiredWorkRate / maxWorkRate_W;
       if (exerciseIntensity > 1) {
         exerciseIntensity = 1;
+        Warning("Desired work rate over max work rate. Proceeding with max work rate.");
       }
       m_PatientActions->GetExercise()->GetIntensity().SetValue(exerciseIntensity);
     } else {
