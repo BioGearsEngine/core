17042418b11e77f5e0e1b268222ca6b5b81b92fe
Nathan Tatum <ntatum@ara.com>
2018-07-31 07:57:21 -0400

    Deleted extraneous commented out portion of code from update

--- projects/biogears/src/engine/Systems/Energy.cpp
+++ projects/biogears/src/engine/Systems/Energy.cpp
@@ -474,11 +474,6 @@ void Energy::CalculateSweatRate()
   if (m_Patient->HasHyperhidrosis()) {
     sweatScalingFactor = m_Patient->GetHyperhidrosis().GetValue();
     sweatRate_kg_Per_s = (1 + sweatScalingFactor) * sweatRate_kg_Per_s;
-    /* if (sweatScalingFactor >= 0) {
-      sweatRate_kg_Per_s = (1 + sweatScalingFactor) * sweatRate_kg_Per_s;
-    } else {
-      sweatRate_kg_Per_s = (1 - sweatScalingFactor) * sweatRate_kg_Per_s;
-    } */
   }
 
   double maxSweatRate_kg_Per_s = 12.5 * m_Patient->GetSkinSurfaceArea().GetValue(AreaUnit::m2) / 60.0 / 1000.0; //10 - 15 g/min/m2
