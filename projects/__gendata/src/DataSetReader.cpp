fe50c26ef4743c725b3d347436e43fc4f987ddb4
Lucas Marin <lmarin@ara.com>
2018-10-24 22:28:41 -0400

    re-doing mistaken initial commit for branch

---
+++ projects/__gendata/src/DataSetReader.cpp
@@ -0,0 +1,15 @@
+#include <biogears/cdm/patient/SEPatient.h>
+#include <biogears/cdm/patient/SENutrition.h>
+#include <biogears/cdm/substance/SESubstance.h>
+#include <biogears/cdm/substance/SESubstanceConcentration.h>
+#include <biogears/cdm/system/environment/SEEnvironmentalConditions.h>
+
+#include <biogears/cdm/utils/FileUtils.h>
+
+bool main(int argc, char* argv)
+{
+  if(argc < 2){
+  	std::cout << 
+  	return false;
+  }
+}
