26b07850f5625e80731905be4673015caeceb850
Steven A White <sawhite@ara.com>
2018-11-05 22:28:35 -0500

    Simplified Command for Generating Data

--- projects/cmd_bio/src/main.cpp
+++ projects/cmd_bio/src/main.cpp
@@ -12,46 +12,35 @@
 #include "data/StabilizationGenerator.h"
 #include "data/SubstanceGenerator.h"
 
-#include "data/NutritionGenerator.h"
 #include "data/CompoundGenerator.h"
+#include "data/NutritionGenerator.h"
 
 int main(int argc, char** argv)
 {
   namespace bg = biogears;
   ArgumentParser parser;
-  parser.parse(argc,argv);
+  parser.parse(argc, argv);
+
   if (argc > 1) {
-	if (parser.exists("STATES")) {
+    if (parser.exists("STATES")) {
       auto generator = bg::StateGenerator();
 
       std::string var1{ argv[2] };
       std::string var2{ " Foo" };
       generator.runScenario(0, var1, var2);
-    } else if (parser.exists("SUBS")) {
-      auto substances = bg::SubstanceGenerator();
-      substances.parse();
-      substances.save();
-    } else if (parser.exists("ENVS")) {
-      auto envs = bg::EnvironmentGenerator();
-      envs.parse();
-      envs.save();
-    } else if (parser.exists("PATIENTS")) {
-      auto patients = bg::PatientGenerator();
-      patients.parse();
-      patients.save();
-    } else if (parser.exists("CONF")) {
-      auto confs = bg::StabilizationGenerator();
-      confs.parse();
-      confs.save();
-    } else if (parser.exists("NUTRITION")) {
-      auto confs = bg::NutritionGenerator();
-      confs.parse();
-      confs.save();
-    } else if (parser.exists("COMPOUNDS")) {
-      auto comps = bg::CompoundGenerator();
-	  comps.parse();
-	  comps.save();
-	} else {
+    } else if (parser.exists("DATA")) {
+      std::vector<std::unique_ptr<bg::CSVToXMLConvertor>> generators;
+      generators.push_back(std::make_unique<bg::SubstanceGenerator>());
+      generators.push_back(std::make_unique<bg::EnvironmentGenerator>());
+      generators.push_back(std::make_unique<bg::PatientGenerator>());
+      generators.push_back(std::make_unique<bg::StabilizationGenerator>());
+      generators.push_back(std::make_unique<bg::NutritionGenerator>());
+      generators.push_back(std::make_unique<bg::CompoundGenerator>());
+      for (auto& gen : generators) {
+        gen->parse();
+        gen->save();
+      }
+    } else {
       std::cout << "Input not recognized" << std::endl;
       std::cout << "Usuage " + std::string(argv[0]) + ": [SUBS|ENVS|PATIENTS|CONF|NEUTRITION]";
     }
