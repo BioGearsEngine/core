01280647d5ec057ad92b355c729103ebe8a5c0ec
Lucas Marin <lmarin@ara.com>
2019-04-24 14:00:31 -0400

    Removed / from beginning of path strings for security

--- share/website/WebsiteGenerationPreProcess.py
+++ share/website/WebsiteGenerationPreProcess.py
@@ -18,19 +18,19 @@ configure/generate with CMake and build.
 
 # This is a list of directories that cmd_bio writes out to when we run scenarios
 # These paths are relative to runtime_dir
-directories = ['/Scenarios/Patient',
-               '/Scenarios/Validation',
-               '/Scenarios/Showcase',
-               '/Scenarios/EnergyEnvironment',
-               '/Scenarios/Compartments',
-               '/Scenarios/Miscellaneous',
-               '/Scenarios/Combined',
-               '/Scenarios/ConsumeMeal',
-               '/Scenarios/Drug',
-               '/Scenarios/AnesthesiaMachine']
+directories = ['Scenarios/Patient',
+               'Scenarios/Validation',
+               'Scenarios/Showcase',
+               'Scenarios/EnergyEnvironment',
+               'Scenarios/Compartments',
+               'Scenarios/Miscellaneous',
+               'Scenarios/Combined',
+               'Scenarios/ConsumeMeal',
+               'Scenarios/Drug',
+               'Scenarios/AnesthesiaMachine']
 
 #runtime_dir is something like d:/biogears/core/build/runtime
-runtime_dir = os.path.dirname(os.path.realpath(__file__))
+runtime_dir = os.path.dirname(os.path.realpath(__file__))+'/'
 os.chdir(runtime_dir)
 print(runtime_dir)
 
@@ -50,11 +50,11 @@ for dir in directories:
 # doxygen looks for the files in Drug/baselines in Validation/baselines, this block of
 # code moves the files so that doxygen can find them
 print("")
-os.chdir(runtime_dir + "/Scenarios")
+os.chdir(runtime_dir + "Scenarios")
 print(os.path.dirname(os.path.realpath(__file__)))
-for file in glob.glob(os.path.join(runtime_dir,'/Scenarios/Drug/baselines/','*.*')):
+for file in glob.glob(os.path.join(runtime_dir,'Scenarios/Drug/baselines/','*.*')):
      print(file)
-     shutil.copy(file,runtime_dir+'/Scenarios/Validation/baselines')
+     shutil.copy(file,runtime_dir+'Scenarios/Validation/baselines')
 
 # There's a bunch of files that we don't seem to regenerate, they're in core/share/website,
 # and get copied into runtime, the following code then moves them all to the directory doxygen looks in
@@ -62,29 +62,29 @@ print("")
 os.chdir(runtime_dir)
 for file in glob.glob(os.path.join(runtime_dir,'*PatientValidationTable.md')):
      print(file)
-     shutil.copy(file,runtime_dir+'/validation')
+     shutil.copy(file,runtime_dir+'validation')
      print("")
 
 os.chdir(runtime_dir)
 for file in glob.glob(os.path.join(runtime_dir,'*ValidationResults.zip')):
      print(file)
-     shutil.copy(file,runtime_dir+'/Scenarios/Validation/baselines')
+     shutil.copy(file,runtime_dir+'Scenarios/Validation/baselines')
      print("")
 
 if not os.path.exists('UnitTests'):
      os.mkdir('UnitTests')
 
-os.chdir(runtime_dir+'/UnitTests')     
+os.chdir(runtime_dir+'UnitTests')     
 if not os.path.exists('BioGearsTests'):
      os.mkdir('BioGearsTests')
 
-os.chdir(runtime_dir+'/UnitTests/BioGearsTests')
+os.chdir(runtime_dir+'UnitTests/BioGearsTests')
 if not os.path.exists('baselines'):
      os.mkdir('baselines')
 
 os.chdir(runtime_dir)
-shutil.copy('RenalTGFFeedbackOutput.zip',runtime_dir+'/UnitTests/BioGearsTests/baselines')
-shutil.copy('SimpleDiffusionFourCompartmentTest.zip',runtime_dir+'/UnitTests/BioGearsTests/baselines')
-shutil.copy('RespiratoryValidationPFT@120.02s.xml',runtime_dir+'/Scenarios/Validation')
+shutil.copy('RenalTGFFeedbackOutput.zip',runtime_dir+'UnitTests/BioGearsTests/baselines')
+shutil.copy('SimpleDiffusionFourCompartmentTest.zip',runtime_dir+'UnitTests/BioGearsTests/baselines')
+shutil.copy('RespiratoryValidationPFT@120.02s.xml',runtime_dir+'Scenarios/Validation')
 
 print("Preprocess successful")
