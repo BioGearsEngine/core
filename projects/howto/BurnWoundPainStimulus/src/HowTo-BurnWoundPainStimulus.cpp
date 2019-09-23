538432e913a8e5a1ffc61ac9761592b64b7c7617
Lucas Marin <lmarin@ara.com>
2019-09-23 13:38:42 -0400

    Updated HowTo-BurnWoundPainStimulus

--- projects/howto/BurnWoundPainStimulus/src/HowTo-BurnWoundPainStimulus.cpp
+++ projects/howto/BurnWoundPainStimulus/src/HowTo-BurnWoundPainStimulus.cpp
@@ -15,6 +15,8 @@ specific language governing permissions and limitations under the License.
 // Include the various types you will be using in your code
 #include <biogears/cdm/utils/SEEventHandler.h>
 #include <biogears/cdm/patient/actions/SESubstanceBolus.h>
+#include <biogears/cdm/patient/actions/SEBurnWound.h>
+#include <biogears/cdm/patient/actions/SESubstanceInfusion.h>
 #include <biogears/cdm/system/physiology/SEBloodChemistrySystem.h>
 #include <biogears/cdm/system/physiology/SECardiovascularSystem.h>
 #include <biogears/cdm/system/physiology/SERespiratorySystem.h>
@@ -22,6 +24,7 @@ specific language governing permissions and limitations under the License.
 #include <biogears/cdm/properties/SEScalarTypes.h>
 #include <biogears/cdm/engine/PhysiologyEngineTrack.h>
 #include <biogears/cdm/compartment/SECompartmentManager.h>
+#include "biogears/cdm/patient/actions/SESubstanceInfusion.h"
 
 using namespace biogears;
 //--------------------------------------------------------------------------------------------------
@@ -35,8 +38,8 @@ using namespace biogears;
 void HowToBurnWoundPainStimulus()
 {
   // Create the engine and load the patient
-	std::unique_ptr<PhysiologyEngine> bg = CreateBioGearsEngine("HowToBolusDrug.log");
-  bg->GetLogger()->Info("HowToBolusDrug");
+	std::unique_ptr<PhysiologyEngine> bg = CreateBioGearsEngine("HowToBurnWoundPainStimulus.log");
+  bg->GetLogger()->Info("HowToBurnWoundPainStimulus");
 	if (!bg->LoadState("./states/StandardMale@0s.xml"))
   {
     bg->GetLogger()->Error("Could not load state, check the error");
@@ -58,7 +61,7 @@ void HowToBurnWoundPainStimulus()
   bg->GetEngineTrack()->GetDataRequestManager().CreatePhysiologyDataRequest().Set("TidalVolume", VolumeUnit::mL);
   bg->GetEngineTrack()->GetDataRequestManager().CreatePhysiologyDataRequest().Set("NeuromuscularBlockLevel");
 
-  bg->GetEngineTrack()->GetDataRequestManager().SetResultsFilename("HowToBolusDrug.csv");
+  bg->GetEngineTrack()->GetDataRequestManager().SetResultsFilename("HowToBurnWoundPainStimulus.csv");
 
 	bg->GetLogger()->Info("The patient is nice and healthy");
 	bg->GetLogger()->Info(std::stringstream() <<"Tidal Volume : " << bg->GetRespiratorySystem()->GetTidalVolume(VolumeUnit::mL) << VolumeUnit::mL);
@@ -70,21 +73,28 @@ void HowToBurnWoundPainStimulus()
 
 	tracker.AdvanceModelTime(50);
 
-	// Get the Succinylcholine substance from the substance manager
-	const SESubstance* succs = bg->GetSubstanceManager().GetSubstance("Succinylcholine");
+  SEBurnWound burnWound;
+  burnWound.GetTotalBodySurfaceArea().SetValue(0.5);
+  burnWound.SetInflammation(true);
+  bg->ProcessAction(burnWound);
 
-  // Create a substance bolus action to administer the substance
-	SESubstanceBolus bolus(*succs);
-	bolus.GetConcentration().SetValue(4820,MassPerVolumeUnit::ug_Per_mL);
-	bolus.GetDose().SetValue(20,VolumeUnit::mL);
-	bolus.SetAdminRoute(CDM::enumBolusAdministration::Intravenous);
-  // BioGears also supports Intramuscular as an admin route as well
-	bg->ProcessAction(bolus);
-	bg->GetLogger()->Info("Giving the patient Succinylcholine.");
+	tracker.AdvanceModelTime(216000);
 
-	tracker.AdvanceModelTime(200);
+	bg->GetLogger()->Info("The patient suffered a burn wound one hour ago");
+	bg->GetLogger()->Info(std::stringstream() <<"Tidal Volume : " << bg->GetRespiratorySystem()->GetTidalVolume(VolumeUnit::mL) << VolumeUnit::mL);
+	bg->GetLogger()->Info(std::stringstream() <<"Systolic Pressure : " << bg->GetCardiovascularSystem()->GetSystolicArterialPressure(PressureUnit::mmHg) << PressureUnit::mmHg);
+	bg->GetLogger()->Info(std::stringstream() <<"Diastolic Pressure : " << bg->GetCardiovascularSystem()->GetDiastolicArterialPressure(PressureUnit::mmHg) << PressureUnit::mmHg);
+	bg->GetLogger()->Info(std::stringstream() <<"Heart Rate : " << bg->GetCardiovascularSystem()->GetHeartRate(FrequencyUnit::Per_min) << "bpm");
+	bg->GetLogger()->Info(std::stringstream() <<"Respiration Rate : " << bg->GetRespiratorySystem()->GetRespirationRate(FrequencyUnit::Per_min) << "bpm");
+	bg->GetLogger()->Info(std::stringstream() <<"Oxygen Saturation : " << bg->GetBloodChemistrySystem()->GetOxygenSaturation());
 
-	bg->GetLogger()->Info("It has been 200s since the administration, not doing well...");
+  SESubstance* rl = bg->GetSubstanceManager().GetSubstance("RingersLactate");
+  SESubstanceInfusion infuse(*rl);
+  bg->ProcessAction(infuse);
+
+  tracker.AdvanceModelTime(216000);
+
+	bg->GetLogger()->Info("The patient was administered RingersLactate one hour ago");
 	bg->GetLogger()->Info(std::stringstream() <<"Tidal Volume : " << bg->GetRespiratorySystem()->GetTidalVolume(VolumeUnit::mL) << VolumeUnit::mL);
 	bg->GetLogger()->Info(std::stringstream() <<"Systolic Pressure : " << bg->GetCardiovascularSystem()->GetSystolicArterialPressure(PressureUnit::mmHg) << PressureUnit::mmHg);
 	bg->GetLogger()->Info(std::stringstream() <<"Diastolic Pressure : " << bg->GetCardiovascularSystem()->GetDiastolicArterialPressure(PressureUnit::mmHg) << PressureUnit::mmHg);
