9319ced3dbe461434a171c9a02279e66673d908e
Matthew McDaniel <mmcdaniel@ara.com>
2019-10-17 10:18:01 -0600

    fix:mmcdaniel-fentanyl:Remove check preventing respiration rate < 4/min
    Delete check that set tidal volume, respiration rate, and ventilation to
    0 if length of respiration period was longer than 0.25 min.  There is
    already a check for target tidal volume = 0.0, so we will not run in to
    any divide by 0 errors without this check.  RR < 4 should be supported
    and should self-police itself.  If you leave it there that long, patient
    will enter irreversible hypoxia, as would be expected.  Re-ran anesthesia machine scenarios and they were not affected by this change.

--- projects/biogears/libBiogears/src/engine/Systems/Respiratory.cpp
+++ projects/biogears/libBiogears/src/engine/Systems/Respiratory.cpp
@@ -1747,14 +1747,6 @@ void Respiratory::CalculateVitalSigns()
     }
   }
 
-  //Zero out if waiting longer than 15 sec
-  //if (m_ElapsedBreathingCycleTime_min > 0.25) {
-  //  GetRespirationRate().SetValue(0.0, FrequencyUnit::Per_min);
-  //  GetTidalVolume().SetValue(0.0, VolumeUnit::L);
-  //  GetTotalAlveolarVentilation().SetValue(0.0, VolumePerTimeUnit::L_Per_min);
-  //  GetTotalPulmonaryVentilation().SetValue(0.0, VolumePerTimeUnit::L_Per_min);
-  //}
-
   /// \todo Move to blood chemistry
   // Although it is called respiratory acidosis/alkalosis, the detection and event setting is actually a part of the @ref BloodChemistrySystem
   // The terms "metabolic" and "respiratory" refer to the origin of the acid-base disturbance
