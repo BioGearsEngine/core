053632cde55b12cab077daf57131a7a82effae1a
Shashank <samshashcool@gmail.com>
2020-06-22 13:23:23 +0530

    Adding Class based CDM2MD and handled whether list_type  or not

--- projects/python/cdm/src/mil/tatrc/physiology/datamodel/doxygen/CDM2MD.py
+++ projects/python/cdm/src/mil/tatrc/physiology/datamodel/doxygen/CDM2MD.py
@@ -1,52 +1,69 @@
-#CDM2MD file for prop.xsd
 import os
 from xml.dom import minidom
+import xmltodict
+import json
 
+class CDM2MD:
+    def __init__(self):
+        self.name=[]
+        self.name_el=[]
+        self.type_el=[]
+        self.files_processed=[]
+        self.tags={}
+        self.list_types=[]
+        
+    def process_sc(self,fout):
+        for input_file in self.files_processed:
+            sc=minidom.parse(input_file)
+            for child in sc.childNodes:
+                if child.nodeName == "xs:schema":
+                    self.parse(child,fout)
 
-def parse(node):
+    def GenComplexType(self,node,fout):
+        xml_json=json.dumps(xmltodict.parse(node.toxml()))
+        json_dict=json.loads(xml_json)
+        c_type=json_dict["xs:complexType"]
+        #print(c_type)
+        c_type_name=c_type["@name"]
+        self.name.append(c_type_name)
+        self.iterdict(c_type)
+        item=list(zip(self.name_el[1:],self.type_el))
+        self.tags[self.name[0]]=item
+        self.name_el=[]
+        self.type_el=[]
+        self.base1=[]
+        self.name=[]
+
+    def iterdict(self,d):
+        for k,v in d.items():
+            if isinstance(v, dict):
+                if k=="xs:element":
+                    name1=v["@name"]
+                    self.name_el.append(name1)
+                    if "@type" in v:
+                        type1=v["@type"]
+                        self.type_el.append(type1)
+                        if "@maxOccurs" in v:
+                            if v["@maxOccurs"]=="unbounded":
+                                self.list_types.append(type1)
+                    else:
+                        self.iterdict(v)
+                elif k=="xs:list":
+                    type1=v["@itemType"]
+                    self.type_el.append("list "+type1)
+
+    def parse(self,node,fout):
         for i in node.childNodes:
-            if i.nodeName == "xs:complexType":
-                GenComplexType(i)
+            self.GenComplexType(i,fout)
 
-def GenComplexType(node):
-    global name,name_el,type_el,tags
-    xml_json=json.dumps(xmltodict.parse(node.toxml()))
-    json_dict=json.loads(xml_json)
-    c_type=json_dict["xs:complexType"]
-    c_type_name=c_type["@name"]
-    name.append(c_type_name)
-    iterdict(c_type)
-    item=list(zip(name_el[1:],type_el))
-    tags[name[0]]=item
-    name_el=[]
-    type_el=[]
-    name=[]
-
-def iterdict(d):
-    for k,v in d.items():        
-        if isinstance(v, dict):
-            if k=="xs:element":
-                name1=v["@name"]
-                name_el.append(name1)
-                if "@type" in v:
-                    type1=v["@type"]
-                    type_el.append(type1)
-                else:
-                    iterdict(v)
-            elif k=="xs:list":
-                type1=v["@itemType"]
-                type_el.append("list "+type1)
-                    
-            iterdict(v)
-                
 if __name__=="__main__":
-    input_file="/opt/biogears/core/share/xsd/cdm/Properties.xsd"
-    sc=minidom.parse(input_file)
-    name=[]
-    name_el=[]
-    type_el=[]
-    tags={}
-    for child in sc.childNodes:
-        if child.nodeName == "xs:schema":
-            parse(child)
+    import sys
+    CDM=CDM2MD()
+    CDM.files_processed.append("./xsd/BioGearsDataModel.xsd")
+    destDir = "./doc/doxygen/processed_md/"
+    #destDir="/home/shashank/Desktop/biogears_tips/"
+    if not os.path.exists(destDir):
+        os.mkdir(destDir)
+    fout=open(os.path.join(destDir,"CDMTables.md"),'w',encoding="utf-8")
+    CDM.process_sc(fout)
 
