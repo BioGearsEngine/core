a9ca9ec9fef03bf3bffe1b7fc2f0adb61cd13ce1
Shashank <samshashcool@gmail.com>
2020-06-23 21:38:44 +0530

    Extracted Extension base tags and improved iterdict to extract all the tags inside document

--- projects/python/cdm/src/mil/tatrc/physiology/datamodel/doxygen/CDM2MD.py
+++ projects/python/cdm/src/mil/tatrc/physiology/datamodel/doxygen/CDM2MD.py
@@ -11,6 +11,8 @@ class CDM2MD:
         self.files_processed=[]
         self.tags={}
         self.list_types=[]
+        self.list_names=[]
+        self.base1=[]
         
     def process_sc(self,fout):
         for input_file in self.files_processed:
@@ -23,12 +25,29 @@ class CDM2MD:
         xml_json=json.dumps(xmltodict.parse(node.toxml()))
         json_dict=json.loads(xml_json)
         c_type=json_dict["xs:complexType"]
-        #print(c_type)
         c_type_name=c_type["@name"]
         self.name.append(c_type_name)
         self.iterdict(c_type)
-        item=list(zip(self.name_el[1:],self.type_el))
-        self.tags[self.name[0]]=item
+        if len(self.base1)==0 and len(self.type_el)==0:
+            self.name.remove(c_type_name)
+        elif len(self.base1)!=0 and len(self.type_el)!=0:
+            item=list(zip(self.name_el[1:],self.type_el))
+            self.tags[self.name[0]]=item
+            if len(self.base1)==1:
+                self.tags[self.name[0]].append(self.base1[0])
+            else:
+                for l in self.base1:
+                    self.tags[self.name[0]].append(l)
+        elif len(self.base1)!=0 and len(self.type_el)==0:
+            self.tags[self.name[0]]=[]
+            if len(self.base1)==1:
+                self.tags[self.name[0]].append(self.base1[0])
+            else:
+                for l in self.base1:
+                    self.tags[self.name[0]].append(l)
+        else:
+            item=list(zip(self.name_el[1:],self.type_el))
+            self.tags[self.name[0]]=item
         self.name_el=[]
         self.type_el=[]
         self.base1=[]
@@ -45,12 +64,26 @@ class CDM2MD:
                         self.type_el.append(type1)
                         if "@maxOccurs" in v:
                             if v["@maxOccurs"]=="unbounded":
+                                #print(d)
                                 self.list_types.append(type1)
                     else:
                         self.iterdict(v)
                 elif k=="xs:list":
                     type1=v["@itemType"]
                     self.type_el.append("list "+type1)
+                elif k=="xs:extension":
+                    base1=v["@base"]
+                    self.base1.append(base1)
+
+                self.iterdict(v)
+
+            elif isinstance(v,list):
+                for i in v:
+                    self.iterdict(i)
+            elif k=="@name":
+                self.name_el.append(v)
+            elif k=="@type":
+                self.type_el.append(v)
 
     def parse(self,node,fout):
         for i in node.childNodes:
@@ -59,11 +92,15 @@ class CDM2MD:
 if __name__=="__main__":
     import sys
     CDM=CDM2MD()
-    CDM.files_processed.append("./xsd/BioGearsDataModel.xsd")
+    CDM.files_processed.append("./xsd/cdm/Properties.xsd")
     destDir = "./doc/doxygen/processed_md/"
     #destDir="/home/shashank/Desktop/biogears_tips/"
     if not os.path.exists(destDir):
         os.mkdir(destDir)
     fout=open(os.path.join(destDir,"CDMTables.md"),'w',encoding="utf-8")
     CDM.process_sc(fout)
-
+    list_type=list(set(CDM.list_types))
+    list_name=list(set(CDM.list_names))
+    CDM.list_types=list_type
+    CDM.list_names=list_name
+ 
