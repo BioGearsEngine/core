bc6016f562ce0127d6dac81cfa101fa2c7fd7496
Shashank <samshashcool@gmail.com>
2020-07-01 20:11:39 +0530

    Adding DoxygenPreprocessor.py completed script

--- projects/python/cdm/src/mil/tatrc/physiology/datamodel/doxygen/DoxygenPreprocessor.py
+++ projects/python/cdm/src/mil/tatrc/physiology/datamodel/doxygen/DoxygenPreprocessor.py
@@ -21,7 +21,39 @@ class DoxygenPreprocessor():
                 except IOError as e:
                     logging.warning("Unable to destination directory {0}. I might not have file permissions".format(destDir))
             try:
-                pass
+                for files in os.listdir(args[0]):
+                    if os.path.isdir(files) or not files.endswith(".md"):
+                        continue
+                    print("Processing file : " + files)
+                    fileprocess=os.path.join(args[0],files)
+                    fileoutput=os.path.join(args[1],files)
+                    with open(fileprocess,'r',encoding="iso-8859-1") as fin, open(fileoutput,'w',encoding="iso-8859-1") as fout:
+                        for line in fin:
+                            #print(line)
+                            if line.startswith("@insert"):
+                                path_file_1=line.partition("@insert")[2].strip(" ").strip("\n")
+                                #print(path_file_1)
+                                if not "/" in path_file_1:
+                                    path_file_1=os.path.join(args[0],path_file_1)
+                                if os.path.isfile(path_file_1):
+                                    with open(path_file_1,'r',encoding="iso-8859-1") as fstub:
+                                        for lines in fstub:
+                                            fout.write(lines)
+                                            #fout.write("\n")
+                                    fstub.close()
+                                else:
+                                    print("Could not find {0} to insert into this page searched [., {0}] ".format(path_file_1))
+                                    fout.write("\n")
+                                    fout.write("<img src=\"./images/MissingTable.jpg\"><center><i>Could not find " + os.path.basename(path_file_1) + "</i></center><br>")
+                            else:
+                                fout.write(line)
+                                #fout.write("\n")
+                    fin.close()
+                    fout.close()
+            except IOError as ex:
+                logging.warning(ex)
+                logging.warning("An Exception has occured with the filesystem. I might not have file permissions")
+
 
 
 
