d6c5139fb50ce0815e48ca8f0321ac50e3b97293
Lucas Marin <lmarin@ara.com>
2021-01-19 10:11:40 -0500

    Added read file or return embedded method

--- projects/biogears/libIO/include/biogears/io/utils.h
+++ projects/biogears/libIO/include/biogears/io/utils.h
@@ -1,12 +1,14 @@
 #pragma once
 
+#include <biogears/io-exports.h>
 #include <string>
 
 namespace biogears {
   namespace io {
-    std::string ResolvePath(const std::string& path);
-    std::string generate_file_sha1( const std::string& path);
-    std::string generate_sha1( char const * buffer, size_t content_size);
+    BIOGEARS_IO_API std::string generate_file_sha1( const std::string& path);
+    BIOGEARS_IO_API std::string generate_sha1( char const * buffer, size_t content_size);
+    BIOGEARS_IO_API std::string ResolvePath(const std::string& path);
+	BIOGEARS_IO_API const char * ReadFileOrReturnEmbedded(char* path,char* fallback_dir,char* embedded_content);
 
   }
 }
--- projects/biogears/libIO/src/utils.cpp
+++ projects/biogears/libIO/src/utils.cpp
@@ -29,6 +29,7 @@ namespace io {
 
   std::string ResolvePath(const std::string& path)
   {
+
     filesystem::path given_path { path };
     filesystem::path cwd { GetCurrentWorkingDirectory() };
     if (path.empty()) {
@@ -86,5 +87,32 @@ namespace io {
     return result;
   }
   //-----------------------------------------------------------------------------
+  const char* ReadFileOrReturnEmbedded(char* path, char* fallback_dir, char* embedded_content)
+  {
+    if (!std::strcmp(path,"")) { // Maybe check the fallback_dir if this hits true
+      return embedded_content;
+    }
+    filesystem::path f_path{ path };
+	char* filePath;
+	if (f_path.exists()) {
+		filePath = path;
+	} else {
+		if (!std::strcmp(fallback_dir,"")) {
+			return embedded_content;
+		}
+		filesystem::path f_fallback_dir { fallback_dir };
+		if (f_fallback_dir.exists()) {
+			filePath = fallback_dir;
+		} else {
+			return embedded_content;
+		}
+	}
+	std::ifstream fileStream;
+	fileStream.open(filePath);
+	std::stringstream stringFileStream;
+	stringFileStream << fileStream.rdbuf();
+	std::string stringFile = stringFileStream.str();
+	return stringFile.c_str();
+  }
 }
 }
