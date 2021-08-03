0ae690033ae520c6799fcffbd9a2cf2c99ae3719
Steven A White <sawhite@ara.com>
2021-08-03 16:07:28 -0400

    f/sawhite-swig - Finalized eposing of EventCallbacks and added a bare bones example of it in the SWIG example

--- projects/biogears/swig_bindings/csharp/src/howto-swig_bindings.cs
+++ projects/biogears/swig_bindings/csharp/src/howto-swig_bindings.cs
@@ -1,11 +1,311 @@
 // Hello World! program
 
 using System;
+using System.Text;
+
 using biogears;
 using CDM = mil.tatrc.physiology.datamodel;
 
 namespace bio
 {
+
+  public class BioGearsEventHandler : biogears.SEEventHandler
+  {
+    static int g_event_count = 0;
+
+    public static string ToString( CDM.enumPatientEvent.value ev ) {
+
+      switch (ev) {
+      case CDM.enumPatientEvent.value.AcuteLungInjury:
+        return "AcuteLungInjury";
+
+      case CDM.enumPatientEvent.value.AcuteRespiratoryDistress:
+        return "AcuteRespiratoryDistress";
+
+      case CDM.enumPatientEvent.value.Antidiuresis:
+        return "Antidiuresis";
+
+      case CDM.enumPatientEvent.value.Asystole:
+        return "Asystoley";
+
+      case CDM.enumPatientEvent.value.Bradycardia:
+        return "Bradycardia";
+
+      case CDM.enumPatientEvent.value.Bradypnea:
+        return "Bradypnea";
+
+      case CDM.enumPatientEvent.value.BrainOxygenDeficit:
+        return "BrainOxygenDeficit";
+
+      case CDM.enumPatientEvent.value.CardiacArrest:
+        return "CardiacArrest";
+
+      case CDM.enumPatientEvent.value.CardiogenicShock:
+        return "CardiogenicShock";
+
+      case CDM.enumPatientEvent.value.CriticalBrainOxygenDeficit:
+        return "CriticalBrainOxygenDeficit";
+
+      case CDM.enumPatientEvent.value.Dehydration:
+        return "Dehydration";
+
+      case CDM.enumPatientEvent.value.Diuresis:
+        return "AcuteLungInjury";
+
+      case CDM.enumPatientEvent.value.Fasciculation:
+        return "Fasciculation";
+
+      case CDM.enumPatientEvent.value.Fatigue:
+        return "Fatigue";
+
+      case CDM.enumPatientEvent.value.FunctionalIncontinence:
+        return "FunctionalIncontinence";
+
+      case CDM.enumPatientEvent.value.HemolyticTransfusionReaction:
+        return "HemolyticTransfusionReaction";
+
+      case CDM.enumPatientEvent.value.Hypercapnia:
+        return "Hypercapnia";
+
+      case CDM.enumPatientEvent.value.Hyperglycemia:
+        return "Hyperglycemia";
+
+      case CDM.enumPatientEvent.value.MildHyperkalemia:
+        return "MildHyperkalemia";
+
+      case CDM.enumPatientEvent.value.SevereHyperkalemia:
+        return "SevereHyperkalemia";
+
+      case CDM.enumPatientEvent.value.MildHypernatremia:
+        return "MildHypernatremia";
+
+      case CDM.enumPatientEvent.value.SevereHypernatremia:
+        return "SevereHypernatremia";
+
+      case CDM.enumPatientEvent.value.Hyperthermia:
+        return "Hyperthermia";
+
+      case CDM.enumPatientEvent.value.Hypoglycemia:
+        return "Hypoglycemia";
+
+      case CDM.enumPatientEvent.value.HypoglycemicShock:
+        return "HypoglycemicShock";
+
+      case CDM.enumPatientEvent.value.HypoglycemicComa:
+        return "HypoglycemicComa";
+
+      case CDM.enumPatientEvent.value.Hypothermia:
+        return "Hypothermia";
+
+      case CDM.enumPatientEvent.value.MildHypokalemia:
+        return "MildHypokalemia";
+
+      case CDM.enumPatientEvent.value.SevereHypokalemia:
+        return "SevereHypokalemia";
+
+      case CDM.enumPatientEvent.value.MildHyponatremia:
+        return "MildHyponatremia";
+
+      case CDM.enumPatientEvent.value.SevereHyponatremia:
+        return "SevereHyponatremia";
+
+      case CDM.enumPatientEvent.value.Hypoxia:
+        return "Hypoxia";
+
+      case CDM.enumPatientEvent.value.HypovolemicShock:
+        return "HypovolemicShock";
+
+      case CDM.enumPatientEvent.value.IntracranialHypertension:
+        return "IntracranialHypertension";
+
+      case CDM.enumPatientEvent.value.IntracranialHypotension:
+        return "IntracranialHypotension";
+
+      case CDM.enumPatientEvent.value.IrreversibleState:
+        return "IrreversibleState";
+
+      case CDM.enumPatientEvent.value.Ketoacidosis:
+        return "Ketoacidosis";
+
+      case CDM.enumPatientEvent.value.LacticAcidosis:
+        return "LacticAcidosis";
+
+      case CDM.enumPatientEvent.value.LiverGlycogenDepleted:
+        return "LiverGlycogenDepleted";
+
+      case CDM.enumPatientEvent.value.MaximumPulmonaryVentilationRate:
+        return "MaximumPulmonaryVentilationRate";
+
+      case CDM.enumPatientEvent.value.MetabolicAcidosis:
+        return "MetabolicAcidosis";
+
+      case CDM.enumPatientEvent.value.MetabolicAlkalosis:
+        return "MetabolicAlkalosis";
+
+      case CDM.enumPatientEvent.value.MuscleCatabolism:
+        return "MuscleCatabolism";
+
+      case CDM.enumPatientEvent.value.MuscleGlycogenDepleted:
+        return "MuscleGlycogenDepleted";
+
+      case CDM.enumPatientEvent.value.MyocardiumOxygenDeficit:
+        return "MyocardiumOxygenDeficit";
+
+      case CDM.enumPatientEvent.value.Natriuresis:
+        return "Natriuresis";
+
+      case CDM.enumPatientEvent.value.NutritionDepleted:
+        return "NutritionDepleted";
+
+      case CDM.enumPatientEvent.value.PulselessRhythm:
+        return "PulselessRhythm";
+
+      case CDM.enumPatientEvent.value.RenalHypoperfusion:
+        return "RenalHypoperfusion";
+
+      case CDM.enumPatientEvent.value.RespiratoryAcidosis:
+        return "RespiratoryAcidosis";
+
+      case CDM.enumPatientEvent.value.RespiratoryAlkalosis:
+        return "RespiratoryAlkalosis";
+
+      case CDM.enumPatientEvent.value.SevereAcuteRespiratoryDistress:
+        return "SevereAcuteRespiratoryDistress";
+
+      case CDM.enumPatientEvent.value.StartOfCardiacCycle:
+        return "AcuteLungInjury";
+
+      case CDM.enumPatientEvent.value.StartOfExhale:
+        return "StartOfExhale";
+
+      case CDM.enumPatientEvent.value.StartOfInhale:
+        return "StartOfInhale";
+
+      case CDM.enumPatientEvent.value.SevereSepsis:
+        return "SevereSepsis";
+
+      case CDM.enumPatientEvent.value.Tachycardia:
+        return "Tachycardia";
+
+      case CDM.enumPatientEvent.value.Tachypnea:
+        return "Tachypnea";
+
+      case CDM.enumPatientEvent.value.TotalPatientEvents:
+        return "TotalPatientevs";
+
+      default:
+        return "";
+
+      }
+
+    }
+
+    public static string ToString( CDM.enumAnesthesiaMachineEvent.value ev ) {
+      switch (ev) {
+      case CDM.enumAnesthesiaMachineEvent.value.OxygenBottle1Exhausted:
+        return "OxygenBottle1Exhausted";
+      case CDM.enumAnesthesiaMachineEvent.value.OxygenBottle2Exhausted:
+        return "OxygenBottle2Exhausted";
+      case CDM.enumAnesthesiaMachineEvent.value.ReliefValveActive:
+        return "ReliefValveActive";
+      default:
+        return "";
+      }
+    }
+
+    public BioGearsEventHandler() : base() {
+    }
+
+    public override void HandlePatientEvent( CDM.enumPatientEvent.value type, bool active, biogears.SEScalarTime time ) {
+      if (g_event_count++ % 31 != 0) {
+        return;
+      }
+
+      Console.WriteLine("\n");
+      Console.WriteLine("  OO O o o o...      _______________________________________ ");
+      Console.WriteLine("  O     ____          |                                      |");
+
+      string ev = ToString(type);
+      var marquee_1 = new StringBuilder("                                      ");
+      var marquee_2 = new StringBuilder("                                      ");
+      string prefix = " ][_n_i_| (   ooo___  |";
+      string end = "|";
+
+
+      string state = (active) ? "(On)" : "(Off)";
+      if (ev.Length < marquee_1.Length) {
+        marquee_1.Remove((marquee_1.Length / 2) - (ev.Length / 2), ev.Length);
+        marquee_1.Insert((marquee_1.Length / 2), ev);
+      } else {
+        marquee_1 = new StringBuilder(ev);
+      }
+      if (state.Length < marquee_2.Length) {
+        marquee_2.Remove((marquee_2.Length / 2) - (state.Length / 2), state.Length);
+        marquee_2.Insert((marquee_2.Length / 2), state);
+      } else {
+        marquee_1 = new StringBuilder(state);
+      }
+      Console.WriteLine(prefix + marquee_1 + end);
+      Console.WriteLine(prefix + marquee_2 + end);
+      Console.WriteLine("(__________|_[______]_|______________________________________|");
+      Console.WriteLine("  0--0--0      0  0      0       0     0        0        0    ");
+      Console.WriteLine("\n");
+    }
+
+    public override void HandlePatientEvent( CDM.enumPatientEvent.value type, bool active ) {
+      var eventTime = new SEScalarTime();
+      eventTime.SetValue(0, TimeUnit.s);
+      HandlePatientEvent(type, active, eventTime);
+    }
+
+    public override void HandleAnesthesiaMachineEvent( CDM.enumAnesthesiaMachineEvent.value type, bool active, biogears.SEScalarTime time ) {
+      if (g_event_count++ % 31 != 0) {
+        return;
+      }
+
+      Console.WriteLine(@"");
+      Console.WriteLine(@"                           (_)");
+      Console.WriteLine(@"            --""-------   0/      ^^");
+      Console.WriteLine(@"  .___...../ /__| |__\ \_/H__,      ^^");
+
+      string ev = ToString(type);
+      var marquee_1 = new StringBuilder(@"                        |");
+      var marquee_2 = new StringBuilder(@"                        /");
+      string prefix_1 =  @"|";
+      string prefix_2 =  @"\";
+      string end_1 = "|";
+      string end_2 = "/";
+
+
+      string state = (active) ? "(On)" : "(Off)";
+      if (ev.Length < marquee_1.Length) {
+        marquee_1.Remove((marquee_1.Length / 2) - (ev.Length / 2), ev.Length);
+        marquee_1.Insert((marquee_1.Length / 2) - (ev.Length / 2), ev);
+      } else {
+        marquee_1 = new StringBuilder(ev);
+      }
+      if (state.Length < marquee_2.Length) {
+        marquee_2.Remove((marquee_2.Length / 2) - (state.Length / 2), state.Length);
+        marquee_2.Insert((marquee_2.Length / 2) - (state.Length / 2), state);
+      } else {
+        marquee_1= new StringBuilder(state);
+      }
+      Console.WriteLine(prefix_1 + marquee_1 + end_1);
+      Console.WriteLine(prefix_2 + marquee_2 + end_2);
+
+      Console.WriteLine(@"#####^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~\O/~~\Q/~^~^~rr\n");
+      Console.WriteLine("\n");
+    }
+
+
+    public override void HandleAnesthesiaMachineEvent( CDM.enumAnesthesiaMachineEvent.value type, bool active ) {
+      var eventTime = new SEScalarTime();
+      eventTime.SetValue(0, TimeUnit.s);
+      HandleAnesthesiaMachineEvent(type, active, eventTime);
+    }
+  };
+
+
   static class csharp_test
   {
     //!
@@ -18,8 +318,9 @@ namespace bio
     //!   4. Apply Actions to an exsiting BioGearsEngine
     //!   5. Create and Apply all BioGears Actions
 
-
     static void Main( string[] args ) {
+
+
       var logger = new Logger("");
       logger.Warning("Perfect Now lets Try to compliate things with two modules!");
 
@@ -38,6 +339,9 @@ namespace bio
       engine.AdvanceModelTime();
       engine.AdvanceModelTimeBy(2.0, TimeUnit.s);
 
+      var eh = new BioGearsEventHandler();
+      engine.SetEventHandler(eh);
+
       var acuteStress = new SEAcuteStress();
       acuteStress.GetSeverity().SetValue(.75);
 
@@ -55,7 +359,6 @@ namespace bio
       m_burnWound.GetTotalBodySurfaceArea().SetValue(50.0 / 100.0);
       engine.ProcessAction(m_burnWound);
       engine.AdvanceModelTimeBy(2.0, TimeUnit.s);
-
 
       var m_substances = engine.GetSubstanceManager();
       var m_albumine = m_substances.GetSubstance("Albumin");
@@ -261,7 +564,7 @@ namespace bio
       engine.AdvanceModelTimeBy(2, TimeUnit.s);
 
       Console.Write("Simulation Finished Press any key to continue");
-			var testString = Console.ReadLine();
+      var testString = Console.ReadLine();
     }
 
 
