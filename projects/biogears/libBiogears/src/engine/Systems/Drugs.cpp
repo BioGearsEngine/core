be344c39062abbdc2c4db3254569760c94d54547
Austin Baird, PhD <ajbaird86@gmail.com>
2022-04-21 15:46:51 -0700

    f/abaird-naloxoneUpdates: validation updates to nasal model for publication

--- projects/biogears/libBiogears/src/engine/Systems/Drugs.cpp
+++ projects/biogears/libBiogears/src/engine/Systems/Drugs.cpp
@@ -449,16 +449,16 @@ void Drugs::AdministerSubstanceNasal()
     //Rate constants in 1/s
     const double nasalk1 = 0.00001736111; //translocation rate constant of unreleased substance from the anterior to the posterior section
     const double nasalk2 = 1000000; // rate constant of release from drug carrier in anterior section
-    const double nasalk3 = 0.00001736111; // translocation rate constant of released substance from the anterior to the posterior section
-    const double nasalk4 = 0.000173; // absorption rate constant in anterior section
-    const double nasalk5 = 0.0011575; // translocation rate constant of unreleased substance from the posterior to the gastrointestinal section
+    const double nasalk3 = 0.000001736111; // translocation rate constant of released substance from the anterior to the posterior section
+    const double nasalk4 = 0.5*(0.000173); // absorption rate constant in anterior section
+    const double nasalk5 = 0.00011575; // translocation rate constant of unreleased substance from the posterior to the gastrointestinal section
     const double nasalk6 = 1000000; // rate constant of release from drug carrier in posterior section
     const double nasalk7 = 0.0011575; // translocation rate constant of released substance from the posterior to the gastrointestinal section
-    const double nasalk8 = 0.0000260; // absorption rate constant in posterior section
+    const double nasalk8 = 0.5*(0.0000260); // absorption rate constant in posterior section
     const double nasalk9 = 1000000; // rate constant of release from drug carrier in gastrointestinal section
-    const double nasalk10 = 0.000000027; // absorption rate constant in gastrointestinal section
-    const double nasalk11 = 0.00001; // rate constant of released drug degradation in anterior section
-    const double nasalk12 = 0.00001; // rate constant of released drug degradation in posterior section
+    const double nasalk10 = 0.0000000027; // absorption rate constant in gastrointestinal section
+    const double nasalk11 = 0.0001; // rate constant of released drug degradation in anterior section
+    const double nasalk12 = 0.0001; // rate constant of released drug degradation in posterior section
     const double nasalk13 = 0.00001; // rate constant of released drug degradation in gastrointestinal section
     const double nasalk14 = 0.000027777; // transit rate constant of unreleased drug through gastrointestinal section
     const double nasalk15 = 0.000027777; // transit rate constant of released drug through gastrointestinal section
