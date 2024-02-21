b6acfbd6b96c1cb1a32845a84056e7ac44cc2fc2
Steven A White <sawhite@ara.com>
2024-02-21 10:25:22 -0500

    Fixes for Retrograde bugs

---
+++ projects/biogears/libBiogears/include/biogears/cdm/patient/actions/SEActionExample.h
@@ -0,0 +1,53 @@
+/**************************************************************************************
+Copyright 2015 Applied Research Associates, Inc.
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this file except in compliance with the License. You may obtain a copy of the License
+at:
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+**************************************************************************************/
+
+#pragma once
+#include <biogears/cdm/patient/actions/SEPatientAction.h>
+
+namespace biogears {
+class SEScalar0To1;
+namespace io {
+  class PatientActions;
+}
+class BIOGEARS_API SEActionExample : public SEPatientAction {
+  friend io::PatientActions;
+
+public:
+  SEActionExample();
+  virtual ~SEActionExample() override;
+
+  static constexpr const char* TypeTag() { return "SEActionExample"; };
+  const char* classname() const override { return TypeTag(); }
+
+  virtual void Clear() override;
+
+  virtual bool IsValid() const override;
+  virtual bool IsActive() const override;
+
+  virtual bool Load(const CDM::ExampleActionData& in);
+  virtual CDM::ExampleActionData* Unload() const override;
+
+  virtual bool HasSeverity() const;
+  virtual SEScalar0To1& GetSeverity();
+
+  virtual void ToString(std::ostream& str) const override;
+ 
+  bool operator==(const SEActionExample& rhs) const;
+  bool operator!=(const SEActionExample& rhs) const;
+
+protected:
+  virtual void Unload(CDM::ExampleActionData& data) const;
+
+private:
+  SEScalar0To1* m_Severity;
+};
+}
