3c981d13900f03f1c051171204e726af81e6f96f
Austin Baird, PhD <ajbaird86@gmail.com>
2025-06-20 12:52:22 -0700

    abaird-getting closer, engine now active around 6m56s not too sure how much better we can do

--- projects/biogears/libBiogears/src/engine/Systems/BloodChemistry.cpp
+++ projects/biogears/libBiogears/src/engine/Systems/BloodChemistry.cpp
@@ -385,7 +385,7 @@ void BloodChemistry::Process()
   m_data.GetSubstances().GetUrea().GetBloodConcentration().Set(m_venaCavaUrea->GetConcentration());
 
 
-  double otherIons_mmol_Per_L = 0.0; //Na, K, and Cl baseline concentrations give SID = 45.83, we assume baseline SID = 40.5, thus "other ions" (i.e. Mg, Ca, Ketones) make up -5.3 mmol_Per_L equivalent of charge
+  double otherIons_mmol_Per_L = -1.02; //Na, K, and Cl baseline concentrations give SID = 45.83, we assume baseline SID = 40.5, thus "other ions" (i.e. Mg, Ca, Ketones) make up -5.3 mmol_Per_L equivalent of charge
   double strongIonDifference_mmol_Per_L = m_venaCavaSodium->GetMolarity(AmountPerVolumeUnit::mmol_Per_L) + m_venaCavaPotassium->GetMolarity(AmountPerVolumeUnit::mmol_Per_L) - (m_venaCavaChloride->GetMolarity(AmountPerVolumeUnit::mmol_Per_L) + (m_venaCavaLactate->GetMolarity(AmountPerVolumeUnit::mmol_Per_L))) + otherIons_mmol_Per_L;
   GetStrongIonDifference().SetValue(strongIonDifference_mmol_Per_L, AmountPerVolumeUnit::mmol_Per_L);
 
