5b4fdd2c17aac2980674f3a9568d0b6a7fbda6cb
Austin Baird, PhD <ajbaird86@gmail.com>
2025-10-28 13:12:12 -0700

    abaird- I think we are there now

--- projects/biogears/libBiogears/src/engine/Systems/Environment.cpp
+++ projects/biogears/libBiogears/src/engine/Systems/Environment.cpp
@@ -629,7 +629,7 @@ void Environment::CalculateRadiation()
     if (clo < 0.1) {
       clo = 0.1; //something stupid to lock in clo
     }
-    m_ClothingToEnclosurePath->GetNextResistance().SetValue(clo * max_result, HeatResistanceUnit::K_Per_W);
+    m_ClothingToEnclosurePath->GetNextResistance().SetValue(0.1*clo * max_result, HeatResistanceUnit::K_Per_W);
 
     //Set the source
     dMeanRadiantTemperature_K = GetConditions().GetMeanRadiantTemperature(TemperatureUnit::K);
@@ -738,7 +738,7 @@ void Environment::CalculateEvaporation()
 
     for (SEThermalCircuitPath* envSkinToGround : m_EnvironmentSkinToGroundPaths) {
       double dClothingResistance_clo = GetConditions().GetClothingResistance(HeatResistanceAreaUnit::clo) * m_cloSegmentation[index];
-      double dClothingResistance_m2_kPa_Per_W = 1000*clo_To_m2_K_Per_W * dClothingResistance_clo / (iCl * dLewisRelation_K_Per_kPa);
+      double dClothingResistance_m2_kPa_Per_W = clo_To_m2_K_Per_W * dClothingResistance_clo / (iCl * dLewisRelation_K_Per_kPa);
       double fCl = 1.0 + 0.3 * dClothingResistance_clo;
       double skinWettednessDiffusion = 0.06;
 
